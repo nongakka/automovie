b1d8dbc1ee1313a89b7000b0b5d327d6401ce880
DOG_GOD <110244508+nongakka@users.noreply.github.com>
2026-03-05 22:47:24 +0700

    Update update.js

--- update.js
+++ update.js
@@ -32,7 +32,7 @@ async function fetchPage(url) {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36",
   "Accept-Language": "en-US,en;q=0.9"
-}
+},
       timeout: 15000,
       validateStatus: () => true
     });
